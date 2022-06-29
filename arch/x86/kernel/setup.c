2977d85a9f70e30b42b650875fe7556481ff51dc
Jason Chen CJ <jason.cj.chen@intel.com>
2022-06-30 01:46:29 +0800

    ANDROID: pkvm: x86: Reserve memory for pKVM
    Reserve required memory for pKVM on Intel platform before de-privilege.
    
    It takes use of general API kvm_hyp_reserve() to do the reservation,
    which get pkvm reserve memory size (hyp_mem_size) from API
    hyp_total_reserve_pages() then record reserved physical memory base to
    hyp_mem_base. At the same time, register system memory block layout
    info to hyp_memory[] array.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    
    dmaluka@ Again, abandon Intel's patches moving arch-agnostic parts of
    arch/arm64/kvm/pkvm.c to virt/kvm/pkvm/pkvm.c for reusing it by pKVM-IA
    in this patch. Instead just add that code to
    arch/x86/kvm/vmx/pkvm/pkvm.c in this patch, independently of the arm64
    code.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    
    Bug: 349990461
    Test: "dmesg | grep pkvm" shows "all cpus are in guest mode!"
    
    Change-Id: I2cb67484918a9757b45eb7b8e35c182fc22c1f69
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -39,6 +39,7 @@
 #include <asm/cpu.h>
 #include <asm/efi.h>
 #include <asm/gart.h>
+#include <asm/kvm_pkvm.h>
 #include <asm/hypervisor.h>
 #include <asm/io_apic.h>
 #include <asm/kasan.h>
@@ -1316,6 +1317,8 @@ void __init setup_arch(char **cmdline_p)
 #endif
 
 	unwind_init();
+
+	kvm_hyp_reserve();
 }
 
 #ifdef CONFIG_X86_32
