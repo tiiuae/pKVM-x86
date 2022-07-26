132120b4d5145696cbeadae7fab5b084f645d066
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-07-26 16:13:56 +0800

    ANDROID: REVERTME: HACK: kvm: pin all the pages used by protected VM
    This is a hack implementation to pin every mappings created for a
    protected VM. It is probably that one page can be counted multiple
    times if KVM is switching EPT root during runtime. All these pinned
    pages will be unpinned after this protected VM is destroyed.
    
    For the proper solution, we would like to leverage the file based memory
    solution used by TDX[1], which the discussion is still on-going at the
    mailing list.
    
    --
    [1]
    https://lore.kernel.org/linux-mm/CAGtprH_83CEC0U-cBR2FzHsxbwbGn0QJ87WFNOEet8sineOcbQ@mail.gmail.com/
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    
    Bug: 349990461
    Test: Android can run protected VMs
    
    Change-Id: I941d5b4544a1aaaa95dc786425f5e57f37f6f9b0
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1796,8 +1796,16 @@ struct kvm_arch_async_pf {
 	bool direct_map;
 };
 
+struct kvm_pinned_page {
+	struct list_head list;
+	struct page *page;
+};
+
 struct kvm_protected_vm {
 	int shadow_vm_handle;
+
+	struct list_head pinned_pages;
+	spinlock_t pinned_page_lock;
 };
 
 extern u32 __read_mostly kvm_nr_uret_msrs;
