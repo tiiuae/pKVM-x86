c6316b51df46b483621fcba725707d895136b109
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-09-27 15:34:54 +0800

    ANDROID: pkvm: x86: Add new hypercall for KVM high to share passthrough dev info
    pKVM needs to know the passthrough device information for a protected VM
    so that can set up the pgstate pgt with proper IOMMU translations. Such
    information is shared through hypercall when KVM high is adding a
    vfio-group. All the devices' BDF in the same IOMMU group will be send
    to pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    
    dmaluka@ Reworked after commit 3e5449d5f954 ("kvm/vfio: Remove
    vfio_group from kvm") and commit 819da99a7360 ("vfio: Hold a reference
    to the iommu_group in kvm for SPAPR").
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    
    Bug: 349990461
    Test: Android can run protected VMs
    
    Change-Id: I1f77d29e81cc2c0dee748e981d7b7b81d319be17
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -2336,4 +2336,6 @@ static inline void kvm_account_pgtable_pages(void *virt, int nr)
 /* Max number of entries allowed for each kvm dirty ring */
 #define  KVM_DIRTY_RING_MAX_ENTRIES  65536
 
+int kvm_arch_add_device_to_pkvm(struct kvm *kvm, struct iommu_group *grp);
+
 #endif
