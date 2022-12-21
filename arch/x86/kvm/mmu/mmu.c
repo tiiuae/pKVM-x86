d47b66aeea779ee9c545cff24d49d0d4e79f75c3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2022-12-21 10:36:30 +0800

    REVERTME: kvm: not to pin the non-memory pfn
    With the passthrough device support in protected VM, passthrough device
    MMIO bar will be mapped in EPT. For such non-memory pfn, should not to
    pin the page.
    
    As this patch is in addition to the REVERTME commit
    "d03dca637960a5f3b5ada0b01cda64596758c18b", also mark with REVERTME so
    that they can be reverted together once the private fd solution is ready.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    (cherry picked from commit db2c09fd51cc65aaf0191148c22ca58154a906ab)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4585,6 +4585,7 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 {
 	kvm_pfn_t orig_pfn;
 	struct kvm_pinned_page *ppage = NULL;
+	struct page *page;
 	int r;
 
 	/* Dummy roots are used only for shadowing bad guest roots. */
@@ -4626,12 +4627,16 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 
 	r = direct_map(vcpu, fault);
 
-	if (ppage && r == RET_PF_FIXED) {
-		ppage->page = pfn_to_page(fault->pfn);
-		get_page(ppage->page);
-		spin_lock(&vcpu->kvm->pkvm.pinned_page_lock);
-		list_add(&ppage->list, &vcpu->kvm->pkvm.pinned_pages);
-		spin_unlock(&vcpu->kvm->pkvm.pinned_page_lock);
+	if (ppage) {
+		if (r == RET_PF_FIXED && (page = kvm_pfn_to_refcounted_page(fault->pfn))) {
+			ppage->page = page;
+			get_page(page);
+			spin_lock(&vcpu->kvm->pkvm.pinned_page_lock);
+			list_add(&ppage->list, &vcpu->kvm->pkvm.pinned_pages);
+			spin_unlock(&vcpu->kvm->pkvm.pinned_page_lock);
+		} else {
+			kfree(ppage);
+		}
 	}
 
 out_unlock:
