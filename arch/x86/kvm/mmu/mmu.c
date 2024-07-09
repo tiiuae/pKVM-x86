cd7ebbdcfe5af5a3e319fe82154bab0a182d74b7
Dmytro Maluka <dmaluka@google.com>
2024-07-09 17:42:49 +0000

    REVERTME: kvm: fix up non-working pinning of protected VM pages
    Since commit 9aa8ab43b381 ("KVM: x86/mmu: Split out TDP MMU page fault
    handling"), direct_page_fault() is not called at all during page fault
    if TDP MMU is used. So need to move the pVM pages pinning hack from
    direct_page_fault() to kvm_tdp_page_fault() to make it have effect.
    
    This is absolutely critical for ensuring correct operation of pVMs.
    Without this patch, for example, it is observed that during a Linux pVM
    teardown, there are multiple unexpected host EPT violations (due to the
    host trying to use pVM pages already slightly before destroying the pVM,
    i.e. when those pages are still donated to the pVM), which results in
    the host instability: random process crashes, kernel panics etc.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    (cherry picked from commit 5ccb2b2497c2e4e8bee7445f76147fa5ea1dd3c8)
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4584,19 +4584,11 @@ static bool is_page_fault_stale(struct kvm_vcpu *vcpu,
 static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 {
 	kvm_pfn_t orig_pfn;
-	struct kvm_pinned_page *ppage = NULL;
-	struct page *page;
 	int r;
 
 	/* Dummy roots are used only for shadowing bad guest roots. */
 	if (WARN_ON_ONCE(kvm_mmu_is_dummy_root(vcpu->arch.mmu->root.hpa)))
 		return RET_PF_RETRY;
-
-	if (vcpu->kvm->arch.vm_type == KVM_X86_PROTECTED_VM) {
-		ppage = kmalloc(sizeof(*ppage), GFP_KERNEL_ACCOUNT);
-		if (!ppage)
-			return -ENOMEM;
-	}
 
 	if (page_fault_handle_page_track(vcpu, fault))
 		return RET_PF_WRITE_PROTECTED;
@@ -4626,18 +4618,6 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 		goto out_unlock;
 
 	r = direct_map(vcpu, fault);
-
-	if (ppage) {
-		if (r == RET_PF_FIXED && (page = kvm_pfn_to_refcounted_page(fault->pfn))) {
-			ppage->page = page;
-			get_page(page);
-			spin_lock(&vcpu->kvm->pkvm.pinned_page_lock);
-			list_add(&ppage->list, &vcpu->kvm->pkvm.pinned_pages);
-			spin_unlock(&vcpu->kvm->pkvm.pinned_page_lock);
-		} else {
-			kfree(ppage);
-		}
-	}
 
 out_unlock:
 	write_unlock(&vcpu->kvm->mmu_lock);
@@ -4751,12 +4731,38 @@ bool kvm_mmu_may_ignore_guest_pat(void)
 
 int kvm_tdp_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 {
+	struct kvm_pinned_page *ppage = NULL;
+	struct page *page;
+	int r;
+
+	if (vcpu->kvm->arch.vm_type == KVM_X86_PROTECTED_VM) {
+		ppage = kmalloc(sizeof(*ppage), GFP_KERNEL_ACCOUNT);
+		if (!ppage)
+			return -ENOMEM;
+	}
+
 #ifdef CONFIG_X86_64
 	if (tdp_mmu_enabled)
-		return kvm_tdp_mmu_page_fault(vcpu, fault);
+		r = kvm_tdp_mmu_page_fault(vcpu, fault);
+	else
+		r = direct_page_fault(vcpu, fault);
+#else
+	r = direct_page_fault(vcpu, fault);
 #endif
 
-	return direct_page_fault(vcpu, fault);
+	if (ppage) {
+		if (r == RET_PF_FIXED && (page = kvm_pfn_to_refcounted_page(fault->pfn))) {
+			ppage->page = page;
+			get_page(page);
+			spin_lock(&vcpu->kvm->pkvm.pinned_page_lock);
+			list_add(&ppage->list, &vcpu->kvm->pkvm.pinned_pages);
+			spin_unlock(&vcpu->kvm->pkvm.pinned_page_lock);
+		} else {
+			kfree(ppage);
+		}
+	}
+
+	return r;
 }
 
 static int kvm_tdp_map_page(struct kvm_vcpu *vcpu, gpa_t gpa, u64 error_code,
