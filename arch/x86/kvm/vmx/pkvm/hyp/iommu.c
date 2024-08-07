c14a28e388c7331aa7cd6ff9835eb7bbff82b17e
Dmytro Maluka <dmaluka@google.com>
2024-08-07 14:24:43 +0000

    ANDROID: pkvm: x86: Make iotlb_lm_invalidate() return value deterministic
    Make sure that iotlb_lm_invalidate() returns failure if sync_shadow_id()
    failed for at least one device.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    
    Bug: 349990461
    Bug: 354228627
    Test: with CONFIG_PKVM_INTEL_DEBUG disabled, start a VM with a
    passthrough PCI device.
    
    Change-Id: I78c9f546c5ce6ee102c971e78dccd92aab7c0038
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu.c
@@ -1684,8 +1684,11 @@ static int iotlb_lm_invalidate(struct pkvm_iommu *iommu, struct qi_desc *desc)
 
 		/* optimization: walk just the needed devices, not the entire bdf space */
 		list_for_each_entry(p, &iommu->ptdev_head, iommu_node)
-			if (p->did == did)
+			if (p->did == did) {
 				ret = sync_shadow_id(iommu, p->bdf, p->bdf + 1, did, NULL);
+				if (ret)
+					break;
+			}
 		break;
 	case DMA_TLB_PSI_FLUSH:
 		data.vaddr = addr & mask;
@@ -1695,8 +1698,11 @@ static int iotlb_lm_invalidate(struct pkvm_iommu *iommu, struct qi_desc *desc)
 
 		/* optimization: walk just the needed devices, not the entire bdf space */
 		list_for_each_entry(p, &iommu->ptdev_head, iommu_node)
-			if (p->did == did)
+			if (p->did == did) {
 				ret = sync_shadow_id(iommu, p->bdf, p->bdf + 1, did, &data);
+				if (ret)
+					break;
+			}
 		break;
 	default:
 		pkvm_err("pkvm: %s: iommu%d: invalid granularity %lld\n",
