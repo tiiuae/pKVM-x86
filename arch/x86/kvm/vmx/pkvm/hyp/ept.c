905ca55de9ed4da6cc2523b0905252feb9ad7346
Markku Ahvenjärvi <mankku@gmail.com>
2025-04-11 13:31:25 +0300

    pkvm: x86: add page table handler for pVM coredump
    As pVM coredump is a debug feature try to minimize the impact to normal
    operation.
    
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- arch/x86/kvm/vmx/pkvm/hyp/ept.c
+++ arch/x86/kvm/vmx/pkvm/hyp/ept.c
@@ -649,7 +649,6 @@ static int pkvm_pgstate_pgt_free_leaf(struct pkvm_pgtable *pgt, unsigned long va
 		flush_data->flushtlb |= true;
 		break;
 	case KVM_X86_PROTECTED_VM: {
-#ifndef CONFIG_PKVM_INTEL_PROTECTED_VM_COREDUMP
 		struct mem_range range;
 		/*
 		 * before returning to host, the memory page previously owned by
@@ -657,7 +656,6 @@ static int pkvm_pgstate_pgt_free_leaf(struct pkvm_pgtable *pgt, unsigned long va
 		 */
 		if (find_mem_range(phys, &range))
 			memset(pgt->mm_ops->phys_to_virt(phys), 0, min(size, range.end - phys));
-#endif
 		pgt->mm_ops->get_page(ptep);
 		ret = __pkvm_host_undonate_guest(phys, pgt, vaddr, size);
 		pgt->mm_ops->put_page(ptep);
