22c7532294ed6b2d233a5501c14991fab60131df
Markku Ahvenjärvi <mankku@gmail.com>
2025-04-15 13:17:37 +0300

    pkvm: x86: validate VM exec controls on vmlaunch
    On a debug build, print VMCS execution control violations when launching a
    guest VM.
    
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- arch/x86/kvm/vmx/pkvm/hyp/nested.c
+++ arch/x86/kvm/vmx/pkvm/hyp/nested.c
@@ -6,6 +6,7 @@
 #include <pkvm.h>
 #include <asm/kvm_pkvm.h>
 #include <capabilities.h>
+#include <vmx/vmx_debug.h>
 #include "pkvm_hyp.h"
 #include "nested.h"
 #include "cpu.h"
@@ -1095,6 +1096,7 @@ int handle_vmlaunch(struct kvm_vcpu *vcpu)
 	if (check_vmx_permission(vcpu))
 		nested_vmx_run(vcpu, true);
 
+	debug_validate_vmcs();
 	return 0;
 }
 
--- arch/x86/kvm/vmx/vmx_debug.h
+++ arch/x86/kvm/vmx/vmx_debug.h
@@ -5,9 +5,13 @@
 #ifdef CONFIG_DEBUG_KERNEL
 char *debug_dump_vmcs(void);
 char *debug_dump_vmx_msr_state(void);
+void debug_validate_vmcs_registers(void);
+#define debug_validate_vmcs debug_validate_vmcs_registers
 #else
+#define debug_validate_vmcs
 static inline void char *debug_dump_vmcs(void) { return NULL; }
 static inline void char *debug_dump_vmx_msr_state(void) { return NULL; }
+static inline void debug_validate_vmcs_registers(void) { return NULL; }
 #endif
 
 #endif /* __KVM_X86_VMX_DEBUG_H */
