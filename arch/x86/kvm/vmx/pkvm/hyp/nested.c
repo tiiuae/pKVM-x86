intel-staging/pKVM-IA

pkvm: x86: Enable Virtualization Exception Information
The virtualization exceptions will save data into the
virtualization-exception information area before being
delivered to the protected VM.

When protected VM gets #VE, it can use hypercall to get the #VE
information, and this provides enough information for protected VM.
Protected VM can use this information to do instruction decode, then use
the IOREAD/IOWRITE hypercall to make KVM do emulation.

This patch defines the struct of #VE information, and write its physical
address to VMCS. Also provides a hypercall GET_VE_INFO for protected VM
to get #VE information.

Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>

arch/x86/kvm/vmx/pkvm/hyp/nested.c
@@ -845,6 +845,15 @@ int handle_vmptrld(struct kvm_vcpu *vcpu)
 						 * this shadow EPTP at last time.
 						 */
 						pkvm_flush_shadow_ept(&shadow_vcpu->vm->sept_desc);
+
+						/*
+						 * Write the #VE information physical address.
+						 */
+						if (shadow_vcpu->vm->vm_type == KVM_X86_PROTECTED_VM) {
+							memset(&shadow_vcpu->ve_info, 0, sizeof(shadow_vcpu->ve_info));
+							vmcs_write64(VE_INFO_ADDR, __pkvm_pa(&shadow_vcpu->ve_info));
+						}
+
 						shadow_vcpu->last_cpu = vcpu->cpu;
 						shadow_vcpu->vmcs02_initied = true;
 					} else {
@@ -854,6 +863,9 @@ int handle_vmptrld(struct kvm_vcpu *vcpu)
 							shadow_vcpu->last_cpu = vcpu->cpu;
 						}
 					}
+
+					pkvm_hvcpu->current_shadow_vcpu = shadow_vcpu;
+
 					copy_shadow_fields_vmcs12_to_vmcs02(vmx, vmcs12);
 					sync_vmcs12_dirty_fields_to_vmcs02(vmx, vmcs12);
 					vmcs_clear_track(vmx, vmcs02);
@@ -867,7 +879,6 @@ int handle_vmptrld(struct kvm_vcpu *vcpu)
 					vmcs_write64(VMCS_LINK_POINTER, __pkvm_pa(vmcs02));
 
 					vmx->nested.current_vmptr = vmptr;
-					pkvm_hvcpu->current_shadow_vcpu = shadow_vcpu;
 
 					nested_vmx_result(VMsucceed, 0);
 				} else {
@@ -1276,6 +1287,26 @@ static bool nested_handle_ept_violation(struct shadow_vcpu_state *shadow_vcpu,
 	return handled;
 }
 
+static void pkvm_get_ve_info(struct kvm_vcpu *vcpu)
+{
+	struct shadow_vcpu_state *shadow_vcpu = to_pkvm_hvcpu(vcpu)->current_shadow_vcpu;
+	struct pkvm_ve_info *ve;
+
+	ve = &shadow_vcpu->ve_info;
+
+	kvm_rcx_write(vcpu, ve->exit_reason);
+	kvm_rdx_write(vcpu, ve->exit_qual);
+	kvm_r8_write(vcpu, ve->gla);
+	kvm_r9_write(vcpu, ve->gpa);
+
+	/*
+	 * When virtualization exception happens, the valid filed in #VE
+	 * information will be set to 0xffffffff. We need to clear it to 0 when
+	 * protected VM handles this #VE, so another #VE can continue to happen.
+	 */
+	ve->valid = 0;
+}
+
 static bool nested_handle_vmcall(struct kvm_vcpu *vcpu)
 {
 	u64 nr, a0, a1, a2, a3;
@@ -1301,6 +1332,10 @@ static bool nested_handle_vmcall(struct kvm_vcpu *vcpu)
 		break;
 	case PKVM_GHC_UNSHARE_MEM:
 		ret = __pkvm_guest_unshare_host(guest_pgt, a0, a1);
+		handled = true;
+		break;
+	case PKVM_GHC_GET_VE_INFO:
+		pkvm_get_ve_info(vcpu);
 		handled = true;
 		break;
 	default:

tiiuae/pKVM-x86

(cherry picked from commit 83542f84ddbe908431d79cac73c16148722555f1)
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

arch/x86/kvm/vmx/pkvm/hyp/nested.c
@@ -850,6 +850,15 @@ int handle_vmptrld(struct kvm_vcpu *vcpu)
 						 * this shadow EPTP at last time.
 						 */
 						pkvm_flush_shadow_ept(&shadow_vcpu->vm->sept_desc);
+
+						/*
+						 * Write the #VE information physical address.
+						 */
+						if (shadow_vcpu->vm->vm_type == KVM_X86_PROTECTED_VM) {
+							memset(&shadow_vcpu->ve_info, 0, sizeof(shadow_vcpu->ve_info));
+							vmcs_write64(VE_INFORMATION_ADDRESS, __pkvm_pa(&shadow_vcpu->ve_info));
+						}
+
 						shadow_vcpu->last_cpu = vcpu->cpu;
 						shadow_vcpu->vmcs02_inited = true;
 					} else {
@@ -859,6 +868,9 @@ int handle_vmptrld(struct kvm_vcpu *vcpu)
 							shadow_vcpu->last_cpu = vcpu->cpu;
 						}
 					}
+
+					pkvm_hvcpu->current_shadow_vcpu = shadow_vcpu;
+
 					copy_shadow_fields_vmcs12_to_vmcs02(vmx, vmcs12);
 					sync_vmcs12_dirty_fields_to_vmcs02(vmx, vmcs12);
 					vmcs_clear_track(vmx, vmcs02);
@@ -872,7 +884,6 @@ int handle_vmptrld(struct kvm_vcpu *vcpu)
 					vmcs_write64(VMCS_LINK_POINTER, __pkvm_pa(vmcs02));
 
 					vmx->nested.current_vmptr = vmptr;
-					pkvm_hvcpu->current_shadow_vcpu = shadow_vcpu;
 
 					nested_vmx_result(VMsucceed, 0);
 				} else {
@@ -1278,6 +1289,26 @@ static bool nested_handle_ept_violation(struct shadow_vcpu_state *shadow_vcpu,
 	return handled;
 }
 
+static void pkvm_get_ve_info(struct kvm_vcpu *vcpu)
+{
+	struct shadow_vcpu_state *shadow_vcpu = to_pkvm_hvcpu(vcpu)->current_shadow_vcpu;
+	struct pkvm_ve_info *ve;
+
+	ve = &shadow_vcpu->ve_info;
+
+	kvm_rcx_write(vcpu, ve->exit_reason);
+	kvm_rdx_write(vcpu, ve->exit_qual);
+	kvm_r8_write(vcpu, ve->gla);
+	kvm_r9_write(vcpu, ve->gpa);
+
+	/*
+	 * When virtualization exception happens, the valid filed in #VE
+	 * information will be set to 0xffffffff. We need to clear it to 0 when
+	 * protected VM handles this #VE, so another #VE can continue to happen.
+	 */
+	ve->valid = 0;
+}
+
 static bool nested_handle_vmcall(struct kvm_vcpu *vcpu)
 {
 	u64 nr, a0, a1, a2, a3;
@@ -1303,6 +1334,10 @@ static bool nested_handle_vmcall(struct kvm_vcpu *vcpu)
 		break;
 	case PKVM_GHC_UNSHARE_MEM:
 		ret = __pkvm_guest_unshare_host(guest_pgt, a0, a1);
+		handled = true;
+		break;
+	case PKVM_GHC_GET_VE_INFO:
+		pkvm_get_ve_info(vcpu);
 		handled = true;
 		break;
 	default: